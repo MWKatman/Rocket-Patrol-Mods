6972ad16c2c17c5b693505934562b2cc98d71253
MWKatman <Mkatlin@ucsc.edu>
2022-06-02 23:08:45 -0700

    all new artwork

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -27,7 +27,7 @@ class Menu extends Phaser.Scene {
         
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL 2.0', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, '←→/A D arrows to move & (F/L) to fire', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, '←→/A D arrows to move & (W/L) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
